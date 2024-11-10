3bb94b2d41a4fdbdacba07343c644a544aa0f098
Jakfut <jakfut0@gmail.com>
2024-11-10 19:03:56 +0100

    AcRunnable updated

--- src/main/kotlin/at/szybbs/tacc/taccbackend/runnable/acRunnable/AcRunnable.kt
+++ src/main/kotlin/at/szybbs/tacc/taccbackend/runnable/acRunnable/AcRunnable.kt
@@ -1,5 +1,6 @@
 package at.szybbs.tacc.taccbackend.runnable.acRunnable
 
+import at.szybbs.tacc.taccbackend.factory.TeslaConnectionFactory
 import org.springframework.scheduling.concurrent.SimpleAsyncTaskScheduler
 import java.time.Instant
 import java.time.LocalDateTime
@@ -9,13 +10,14 @@ import java.util.*
 
 // see flow diagram v0.2
 class AcRunnable(
-    private val vin: String,
     private val userId: UUID,
-    private val targetState: Boolean
+    private val targetState: Boolean,
+    private val teslaConnectionFactory: TeslaConnectionFactory
 ) : Runnable {
     override fun run() {
-        // TODO implement
-        println("ACRunnable: $vin, $userId, $targetState")
+        val teslaConnectionClient = teslaConnectionFactory.createTeslaConnectionClient(userId)
+
+        teslaConnectionClient.changeAcState(targetState)
     }
 }
 
