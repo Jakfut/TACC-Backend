53eb57964c9dd0e93434a4d7aec3da77d3d04908
Jakfut <jakfut0@gmail.com>
2025-01-22 11:41:49 +0100

    working OAuth2GrantController

---
+++ src/main/kotlin/at/szybbs/tacc/taccbackend/controller/OAuth2GrantController.kt
@@ -0,0 +1,25 @@
+package at.szybbs.tacc.taccbackend.controller
+
+import at.szybbs.tacc.taccbackend.service.UserInformationService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+
+
+@RestController
+class OAuth2GrantController(
+    private val userInformationService: UserInformationService,
+)
+{
+    @GetMapping("/auth/google/start/{user-information-id}")
+    fun handleGoogleRedirect(
+        @PathVariable("user-information-id") userInformationId: UUID
+    ): String {
+        val generatedSession = UUID.randomUUID().toString()
+
+        userInformationService.setOauth2Session(userInformationId,generatedSession)
+
+        return "tacc.jakfut.at/oauth2/authorization/google?session_id=$generatedSession"
+    }
+}
